84b47fae42db943a21e990dc6fdc2fdcaa12d1a5
ademola-lou <34609417+ademola-lou@users.noreply.github.com>
2025-10-02 06:52:35 +0000

    fix lint errors

--- examples/jsm/tsl/display/MeshBlendNode.js
+++ examples/jsm/tsl/display/MeshBlendNode.js
@@ -1,46 +1,53 @@
-import { TempNode, MeshBasicNodeMaterial, RenderTarget, RGBAFormat, NearestFilter, QuadMesh, Vector2 } from 'three/webgpu';
-import { nodeObject, vec4, vec3, float, modelPosition, modelWorldMatrix, passTexture, hash, Fn, wgslFn, NodeUpdateType, texture, screenUV } from 'three/tsl';
+import { TempNode, MeshBasicNodeMaterial, RenderTarget, QuadMesh, Vector2 } from 'three/webgpu';
+import { nodeObject, vec4, vec3, float, modelPosition, modelWorldMatrix, Fn, wgslFn, NodeUpdateType, texture, screenUV } from 'three/tsl';
 
 //Source: https://www.jacktollenaar.top/mesh-seam-smoothing-blending#h.50wag6hqg9gh
 
 const _size = /*@__PURE__*/ new Vector2();
 
 class MeshBlendNode extends TempNode {
-    constructor(sceneOutputNode, sceneDepthNode, camera, scene) {
-        super('vec4');
-        this.sceneOutputNode = sceneOutputNode;
-        this.sceneDepthNode = sceneDepthNode;
-        this.updateBeforeType = NodeUpdateType.FRAME;
-        this.renderTarget = new RenderTarget(1, 1);
-        this.mainCamera = camera;
-        this.mainScene = scene;
-        this.blendFactor = float(1.2);
-        this.kernelSize = float(5);
-        this.kernelRadius = float(0.01 * this.blendFactor.value);   
-        this.depthFalloff = float(0.0001 * this.blendFactor.value);
-        this.debugMaterial = new MeshBasicNodeMaterial();
-        this._quadMesh = new QuadMesh(this.debugMaterial);
-    }
 
-    setup(){
-         const CustomHash = wgslFn(`
+	constructor( sceneOutputNode, sceneDepthNode, camera, scene ) {
+
+		super( 'vec4' );
+		this.sceneOutputNode = sceneOutputNode;
+		this.sceneDepthNode = sceneDepthNode;
+		this.updateBeforeType = NodeUpdateType.FRAME;
+		this.renderTarget = new RenderTarget( 1, 1 );
+		this.mainCamera = camera;
+		this.mainScene = scene;
+		this.blendFactor = float( 1.2 );
+		this.kernelSize = float( 5 );
+		this.kernelRadius = float( 0.01 * this.blendFactor.value );
+		this.depthFalloff = float( 0.0001 * this.blendFactor.value );
+		this.debugMaterial = new MeshBasicNodeMaterial();
+		this._quadMesh = new QuadMesh( this.debugMaterial );
+
+	}
+
+	setup() {
+
+		const CustomHash = wgslFn( `
             fn Hash(p: vec3f) -> f32 {
 
                 var lp = (fract(p * 0.3183099 + 0.1));
                 lp *= 17.0;
                 return fract(lp.x * lp.y * lp.z * (lp.x + lp.y + lp.z));
             }
-        `)
-        this.hashShader = Fn( () => {
-            let p = vec3(modelWorldMatrix.mul(vec3(modelPosition))).toVar();
-            return vec4(CustomHash(p), 0., 0., 1.);
-        });
-        this.hashMaterial = new MeshBasicNodeMaterial();
-        this.hashMaterial.colorNode = this.hashShader();
+        ` );
+		this.hashShader = Fn( () => {
 
-        const uv = screenUV;
-        const FinalOutputNode = Fn(()=>{
-            const outputPassFunc1 = wgslFn(`
+			const p = vec3( modelWorldMatrix.mul( vec3( modelPosition ) ) ).toVar();
+			return vec4( CustomHash( p ), 0., 0., 1. );
+
+		} );
+		this.hashMaterial = new MeshBasicNodeMaterial();
+		this.hashMaterial.colorNode = this.hashShader();
+
+		const uv = screenUV;
+		const FinalOutputNode = Fn( ()=>{
+
+			const outputPassFunc1 = wgslFn( `
                 fn OutputPassFunc1(sceneDepth: vec4<f32>, tex: texture_2d<f32>, sampler: sampler, uv: vec2f, kernelSize: f32, kernelRadius: f32, depthFalloff: f32) -> vec4<f32> {
                     var seamLocation = vec2<f32>(0., 0.);
                     var minDist = f32(9999999.);
@@ -64,9 +71,9 @@ class MeshBlendNode extends TempNode {
                     
                     return vec4<f32>(seamLocation.x, seamLocation.y, minDist, 1.);
                 }
-            `)
+            ` );
 
-            const finalPass = wgslFn(`
+			const finalPass = wgslFn( `
                 fn FinalPass(sceneColor: vec4f, mirroredColor: vec4f, seamLocation: vec2f, kernelRadius: f32, sceneDepth: vec4f, otherDepth: vec4f, depthFalloff: f32, minDist: f32) -> vec4f {
                     
                     let depthDiff = abs(otherDepth.r - sceneDepth.r);
@@ -78,47 +85,56 @@ class MeshBlendNode extends TempNode {
     
                     return mix(sceneColor, mirroredColor, finalWeight);
                 }
-            `);
+            ` );
 
-            const pass1 = outputPassFunc1(
-                texture(this.sceneDepthNode, uv),
-                texture(this.renderTarget.textures[0], uv), 
-                texture(this.sceneOutputNode, uv), 
-                uv, this.kernelSize, this.kernelRadius, this.depthFalloff);
+			const pass1 = outputPassFunc1(
+				texture( this.sceneDepthNode, uv ),
+				texture( this.renderTarget.textures[ 0 ], uv ),
+				texture( this.sceneOutputNode, uv ),
+				uv, this.kernelSize, this.kernelRadius, this.depthFalloff );
 
-            const mirroredColor = texture(this.sceneOutputNode, uv.add(pass1.xy.mul(2.)));
-            const otherDepth = texture(this.sceneDepthNode, uv.add(pass1.xy.mul(2.)));
+			const mirroredColor = texture( this.sceneOutputNode, uv.add( pass1.xy.mul( 2. ) ) );
+			const otherDepth = texture( this.sceneDepthNode, uv.add( pass1.xy.mul( 2. ) ) );
 
-            const sceneColor = texture(this.sceneOutputNode, uv);
-            const sceneDepth = texture(this.sceneDepthNode, uv);
-            return finalPass(sceneColor, mirroredColor, pass1.xy, this.kernelRadius, sceneDepth, otherDepth, this.depthFalloff, pass1.z);
-        })();
-        return FinalOutputNode;
-    }
+			const sceneColor = texture( this.sceneOutputNode, uv );
+			const sceneDepth = texture( this.sceneDepthNode, uv );
+			return finalPass( sceneColor, mirroredColor, pass1.xy, this.kernelRadius, sceneDepth, otherDepth, this.depthFalloff, pass1.z );
 
-    setSize(width, height){
-        this.renderTarget.setSize(width, height);
-    }
+		} )();
+		return FinalOutputNode;
 
-    updateBefore(frame){
-        const { renderer } = frame;
-        const size = renderer.getSize( _size );
+	}
+
+	setSize( width, height ) {
+
+		this.renderTarget.setSize( width, height );
+
+	}
+
+	updateBefore( frame ) {
+
+		const { renderer } = frame;
+		const size = renderer.getSize( _size );
 		this.setSize( size.width, size.height );
 
-        this.mainScene.overrideMaterial = this.hashMaterial;
-        renderer.setRenderTarget(this.renderTarget);
-        renderer.render(this.mainScene, this.mainCamera);
+		this.mainScene.overrideMaterial = this.hashMaterial;
+		renderer.setRenderTarget( this.renderTarget );
+		renderer.render( this.mainScene, this.mainCamera );
 
-        this.mainScene.overrideMaterial = null;
-        renderer.setRenderTarget(null);
-        this._quadMesh.render(renderer);
-    }
+		this.mainScene.overrideMaterial = null;
+		renderer.setRenderTarget( null );
+		this._quadMesh.render( renderer );
 
-    dispose(){
-        this.renderTarget.dispose();
-        this.hashMaterial.dispose();
-        this.debugMaterial.dispose();
-    }
+	}
+
+	dispose() {
+
+		this.renderTarget.dispose();
+		this.hashMaterial.dispose();
+		this.debugMaterial.dispose();
+
+	}
+
 }
 
 export const meshblend = ( sceneOutputNode, sceneDepthNode, camera, scene ) => nodeObject( new MeshBlendNode( sceneOutputNode, sceneDepthNode, camera, scene ) );
